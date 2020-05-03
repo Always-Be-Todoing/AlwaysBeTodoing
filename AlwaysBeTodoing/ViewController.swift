29898a859fef6955e152739b01e790f895c832aa
jfleshma <jfles003@odu.edu>
2020-05-03 09:54:11 -0500

    Initial Commit

---
+++ AlwaysBeTodoing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlwaysBeTodoing
+//
+//  Created by Jeremy Fleshman on 5/3/20.
+//  Copyright © 2020 Jeremy Fleshman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
